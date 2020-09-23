61c5d4d6e5bf268affa83363993eea09af8bec76
BAH--zer <BAH--zer@ya.ru>
2020-09-23 14:29:02 +0500

    сделал инструкцию по настройке

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -8,9 +8,9 @@ class DatabaseSeeder extends Seeder{
      * @return void
      */
     public function run() {
-        //$this->call('TestTableSeeder');		$this->command->info('Тестовая таблица загружена данными!');
-		//$this->call('UserRootSeeder');		$this->command->info('Администратор и гость установлены!');
-		//$this->call('ObjStructSeeder');		$this->command->info('Тестовая структура системы заведена!');
+        $this->call('TestTableSeeder');		$this->command->info('Тестовая таблица загружена данными!');
+		$this->call('UserRootSeeder');		$this->command->info('Администратор и гость установлены!');
+		$this->call('ObjStructSeeder');		$this->command->info('Тестовая структура системы заведена!');
 		$this->call('TestNSDSeeder');		$this->command->info('Тестовая структура НСИ заведена!');
     }
 }
